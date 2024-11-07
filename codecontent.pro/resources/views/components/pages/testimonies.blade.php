01b41addf37ca494135fa5887c930cedd3b13260
alloyking1 <alloyking1@gmail.com>
2024-11-08 00:07:53 +0100

    feature:testimonials

--- codecontent.pro/resources/views/components/pages/testimonies.blade.php
+++ codecontent.pro/resources/views/components/pages/testimonies.blade.php
@@ -19,17 +19,17 @@
         </div>
         <div class="grid md:grid-cols-2 gap-2 md:p-16 p-4">
             <div class="mx-auto my-auto">
-                <p class="text-5xl font-bold">company name</p>
+                <p class="text-5xl font-bold">Laraveldev.pro</p>
                 <h2 class="text-3xl font-bold py-16">
-                    “We launched on beehiiv with nothing, scaled to over 250,000 subscribers, and got acquired all in under a year. beehiiv is that powerful.”
+                    “Working with CodeContent was a game-changer for our business. Moses and his team delivered a high-quality, custom website that perfectly captures our brand. Their expertise in PHP, Laravel, and no-code tools made the whole process smooth and efficient. The result is a stunning, easy-to-manage site that our customers love. Highly recommended!"
                 </h2>
-                <p class="text-2xl font-bold">Name</p>
-                <p class="font-thin">Position</p>
+                <p class="text-2xl font-bold">Keshi</p>
+                <p class="font-thin">CEO</p>
             </div>
-            <div class="hidden md:block mx-auto my-auto">
+            <div class="hidden md:block mx-auto my-auto border rounded-full p-4 bg-white">
                 <img 
-                class="w-auto h-auot rounded-full"
-                src="https://images.pexels.com/photos/5271121/pexels-photo-5271121.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
+                class="w-auto h-[50%] rounded-full"
+                src="https://images.pexels.com/photos/7752895/pexels-photo-7752895.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
                 alt="">
             </div>
         </div>
